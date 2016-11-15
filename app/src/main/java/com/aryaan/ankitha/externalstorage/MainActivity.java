e0c93e4a61eaa719c0571c3dc20fa0a40c388265
AnkithaP <ankitha.parameswara@gmail.com>
2016-11-15 17:01:38 +0530

    Initial commit

---
+++ app/src/main/java/com/aryaan/ankitha/externalstorage/MainActivity.java
@@ -0,0 +1,84 @@
+package com.aryaan.ankitha.externalstorage;
+
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+    }
+
+    public void saveInternalCache(View view){
+        String data = editText.getText().toString();
+        File folder = getCacheDir();
+        File myFile = new File(folder,"mydata1.txt");
+        writeData(myFile,data);
+    }
+
+    public void saveExternalCache(View view){
+        String data = editText.getText().toString();
+        File folder = getExternalCacheDir();
+        File myFile = new File(folder,"mydata2.txt");
+        writeData(myFile,data);
+    }
+
+    public void saveExternalPrivate(View view){
+        String data = editText.getText().toString();
+        File folder = getExternalFilesDir("AryaanS");
+        File myFile = new File(folder,"mydata3.txt");
+        writeData(myFile,data);
+    }
+
+    public void saveExternalPublic(View view){
+        String data = editText.getText().toString();
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File myFile = new File(folder,"mydata4.txt");
+        writeData(myFile,data);
+    }
+
+    private void writeData(File myFile,String data){
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(myFile);
+            fileOutputStream.write(data.getBytes());
+            message(data+"saved successfully"+myFile.getAbsolutePath());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (fileOutputStream != null){
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void next(View view){
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void message(String message){
+        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/aryaan/ankitha/externalstorage/SecondActivity.java
@@ -0,0 +1,101 @@
+package com.aryaan.ankitha.externalstorage;
+
+import android.content.Intent;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class SecondActivity extends AppCompatActivity {
+
+    EditText displayText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        displayText = (EditText)findViewById(R.id.editText2);
+    }
+
+    public void loadInternalCache(View view){
+        File folder = getCacheDir();
+        File myFile = new File(folder,"mydata1.txt");
+        String data = readData(myFile);
+        if (data != null){
+            displayText.setText(data);
+        }else{
+            displayText.setText("No data found");
+        }
+    }
+
+    public void loadExternalCache(View view){
+        File folder = getExternalCacheDir();
+        File myFile = new File(folder,"mydata2.txt");
+        String data = readData(myFile);
+        if (data != null){
+            displayText.setText(data);
+        }else{
+            displayText.setText("No data found");
+        }
+    }
+
+    public void loadExternalPrivate(View view){
+        File folder = getExternalFilesDir("AryaanS");
+        File myFile = new File(folder,"mydata3.txt");
+        String data = readData(myFile);
+        if (data != null){
+            displayText.setText(data);
+        }else{
+            displayText.setText("No data found");
+        }
+    }
+
+    public void loadExternalPublic(View view){
+        File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        File myFile = new File(folder,"mydata4.txt");
+        String data = readData(myFile);
+        if (data != null){
+            displayText.setText(data);
+        }else{
+            displayText.setText("No data found");
+        }
+    }
+
+    private String readData(File myFile){
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(myFile);
+            int read = -1;
+            StringBuffer stringBuffer = new StringBuffer();
+            while((read=fileInputStream.read()) == -1){
+                stringBuffer.append((char)read);
+            }
+            return stringBuffer.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (fileInputStream != null){
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    public void previous(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
